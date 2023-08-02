de26a6271578e5bfee647dcc61a91c3402b67a43
Kanza Jamal <skanzajamal@gmail.com>
2023-08-02 20:58:34 +0200

    multi threading

---
+++ src/main/java/FastFoodRestaurant.java
@@ -0,0 +1,42 @@
+public class FastFoodRestaurant {
+
+    private String lastName;
+    public int noOfBurgerSold = 0;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getNoOfBurgerSold() {
+        return noOfBurgerSold;
+    }
+
+    public void setNoOfBurgerSold(int noOfBurgerSold) {
+        this.noOfBurgerSold = noOfBurgerSold;
+    }
+
+    public void buyBurger(String clientName) {
+
+        threading();
+        System.out.println(clientName + "bought a burger");
+
+        // prevent from race condition
+        synchronized (this){
+            this.lastName = clientName;
+            noOfBurgerSold++;
+        }
+    }
+
+    public void threading() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/main/java/MultiThreading.java
@@ -0,0 +1,26 @@
+public class MultiThreading {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        long startTime = System.currentTimeMillis();
+        FastFoodRestaurant fastFoodRestaurant = new FastFoodRestaurant();
+
+        Thread t1 = new Thread(()-> fastFoodRestaurant.buyBurger("Customer1"));
+        Thread t2 = new Thread(()-> fastFoodRestaurant.buyBurger("Customer2"));
+        Thread t3 = new Thread(()-> fastFoodRestaurant.buyBurger("Customer3"));
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+        //wait for other threads to complete the task
+        t1.join();
+        t2.join();
+        t3.join();
+
+        System.out.println("Total number of burgers sold: " + fastFoodRestaurant.getNoOfBurgerSold());
+        System.out.println("Last name of client: " + fastFoodRestaurant.getLastName());
+        System.out.println("Total execution time " + (System.currentTimeMillis() - startTime) + " in milliseconds");
+
+    }
+}
